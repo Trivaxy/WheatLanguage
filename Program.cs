6dac8df5383d991fed8e5795f4179dfa55bc8df1
Trivaxy <itsmeomar2003@yahoo.com>
2021-01-22 00:46:20 +0200

    Update program.cs to match callbacks

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Text;
 
 namespace WheatLanguage
 {
@@ -9,22 +7,50 @@ namespace WheatLanguage
 		static void Main(string[] args)
 		{
 			string s = @"
-put 25 grain in bag a
-put 4 grain in bag b
-
+put 9 grain in bag a
+put 3 grain in bag b
 sweep in bag g
-grow bag b
-scoop in bag b
+
+grow bag a
+scoop in bag f
+sweep in bag a
+
+dump bag b
+put 1 grain in bag g
+sweep in bag b
+
+grow bag g
 sweep in bag g
 
 grow bag a
 scoop in bag a
 sweep in bag c
+
 dump bag g
 put 1 grain in bag d
 sweep in bag g
 
-if bag d weight lessthan bag b weight sleep 10 hours
+if bag d weight lessthan bag f weight sleep 29 hours
+
+dump bag a
+sweep in bag g
+
+grow bag c
+scoop in bag a
+sweep in bag c
+
+dump bag g
+dump bag d
+put 1 grain in bag e
+sweep in bag g
+
+if bag e weight lessthan bag b weight sleep 20 hours
+
+dump bag a
+put 1 grain in bag b
+sweep in bag g
+
+pour bag f in bag a
 
 announce bag a
 announce bag b
@@ -34,14 +60,11 @@ announce bag c
 			Lexer lexer = new Lexer(s);
 			Parser parser = new Parser(lexer.Tokenize());
 
-			StringBuilder sb = new StringBuilder();
-			StringWriter writer = new StringWriter(sb);
-
 			Runtime runtime = new Runtime(
 				parser.AssembleTokens(),
 				200,
-				writer,
-				("a", 2),
+				Console.WriteLine,
+				("a", 3),
 				("b", 4),
 				("c", 8),
 				("d", 16),
@@ -51,8 +74,6 @@ announce bag c
 			);
 
 			runtime.Execute();
-
-			Console.WriteLine(sb.ToString());
 		}
 
 		public static bool Error(string message)
